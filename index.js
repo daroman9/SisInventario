8e638a2b7066e48745112bf38ebb336875606d3c
Deiby Roman <deibyalejandro42@hotmail.com>
2020-02-11 11:27:53 -0500

    Final

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-import express from 'express';
+import express from "express";
 //const express=require('express');
-import morgan from 'morgan';
+import morgan from "morgan";
 //const morgan=require('morgan');
-import cors from 'cors';
+import cors from "cors";
 //const cors=require('cors');
-import path from 'path';
-import mongoose from 'mongoose';
-import router from './routes';
+import path from "path";
+import mongoose from "mongoose";
+import router from "./routes";
 
 //Conexión a la base de datos MongoDB
-mongoose.Promise=global.Promise;
-const dbUrl = 'mongodb://localhost:27017/dbsistema';
-mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
-.then(mongoose => console.log('Conectado a la BD en el puerto 27017'))
-.catch(err => console.log(err));
+mongoose.Promise = global.Promise;
+const dbUrl =
+  "mongodb+srv://daroman9:YR69oHjgBxJisFmp@cluster0-peclw.mongodb.net/dbsistema?retryWrites=true&w=majority";
 
+//const dbUrl = 'mongodb://localhost:27017/dbsistema';
+mongoose
+  .connect(dbUrl, { useCreateIndex: true, useNewUrlParser: true })
+  .then(mongoose => console.log("Conectado a la BD en el puerto 27017"))
+  .catch(err => console.log(err));
 
-const app=express();
-app.use(morgan('dev'));
+const app = express();
+app.use(morgan("dev"));
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api',router);
-app.set('port',process.env.PORT || 3000);
+app.use("/api", router);
+app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get('port'),()=>{
-    console.log('server on port ' + app.get('port'));
-}); 
+app.listen(app.get("port"), () => {
+  console.log("server on port " + app.get("port"));
+});
